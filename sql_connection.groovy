7dee04406e382ccc64e47ea3631d97dc2f75f0dc
Bayo Erinle <berinle@gmail.com>
2010-04-18 06:46:10 -0400

    starting random thoughts on groovy

---
+++ sql_connection.groovy
@@ -0,0 +1,21 @@
+@Grapes([
+	@Grab('org.slf4j:slf4j-simple:1.5.11'),
+	@Grab('mysql:mysql-connector-java:5.1.12'),
+	@GrabConfig(systemClassLoader = true)
+])
+
+import org.slf4j.*
+import groovy.sql.*
+
+def log = LoggerFactory.getLogger('samplescript')
+
+log.info 'Initialize SQL'
+
+def uname = args[0]
+def pass = args[1]
+
+def sql = Sql.newInstance('jdbc:mysql://localhost/cbp', 'com.mysql.jdbc.Driver')
+
+log.info 'Got sql connection ${sql}'
+
+log.info 'auto commit status: ${sql.autoCommit}'
